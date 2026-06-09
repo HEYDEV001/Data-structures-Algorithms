18e442cb285450c410aaaaddf08fe440dd368792
Dev pathak <devpathak9685@gmail.com>
2026-06-09 15:36:09 +0530

    Sorting - BubbleSort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,23 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 3, 6, 7, 8, 9, 0};
+        bubbleSort(arr);
+
+    }
+    public static void bubbleSort(int[] arr) {
+        for(int i = 0; i < arr.length -1; i++) {
+            for(int j = 0; j < arr.length - 1 - i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp  = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("The Sorted Array is: "+ Arrays.toString(arr));
+    }
+}
