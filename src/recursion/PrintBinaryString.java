07f29d96736a5a10dfb2e2e7e48afea455f44e7a
Dev pathak <devpathak9685@gmail.com>
2026-06-18 23:17:00 +0530

    Recursion - Print Binary String Of Size N Without Consicutive 1s

---
+++ src/recursion/PrintBinaryString.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class PrintBinaryString {
+    public static void main(String[] args) {
+        printBinaryString(3, 0, "");
+
+    }
+    public static void printBinaryString(int n, int last, String str) {
+        if (n==0) {
+            System.out.println(str);
+            return;
+        }
+        printBinaryString(n-1, 0, str+"0");
+        if(last==0) {
+            printBinaryString(n-1, 1, str+"1");
+        }
+    }
+}
