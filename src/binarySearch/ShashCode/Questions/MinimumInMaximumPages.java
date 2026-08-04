4855e3f38e8e780da0a2f5ee812bfcb1ab83bae1
Dev pathak <devpathak9685@gmail.com>
2026-08-04 21:53:38 +0530

    Binary Search(ShashCode)  - (QUESTIONS)-> (GeeksForGeeks MinimumInMaximumPages)

---
+++ src/binarySearch/ShashCode/Questions/MinimumInMaximumPages.java
@@ -0,0 +1,48 @@
+package binarySearch.ShashCode.Questions;
+
+public class MinimumInMaximumPages {
+    public static void main(String[] args) {
+
+    }
+    public int findPages(int[] arr, int k) {
+        // code here
+
+        if(k> arr.length){
+            return -1;
+        }
+        int start=0;
+        int end =0;
+        for(int a : arr){
+            start = Math.max(start,a);
+            end+=a;
+        }
+        int ans =-1;
+        while(start<=end){
+            int mid = start+((end-start)/2);
+            if(isAllocationPossible(arr, mid, k)){
+                ans  = mid;
+                end  = mid-1;
+            }else{
+                start = mid +1;
+
+            }
+        }
+        return ans;
+    }
+
+    public boolean isAllocationPossible(int[] arr, int maxPages, int students){
+        int studentCount = 1;
+        int pages =0;
+        for(int a : arr){
+            pages+=a;
+            if(pages>maxPages){
+                pages = a;
+                studentCount++;
+            }
+            if(studentCount > students){
+                return false;
+            }
+        }
+        return true;
+    }
+}
