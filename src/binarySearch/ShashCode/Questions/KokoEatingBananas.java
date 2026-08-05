e6de5ad36562e45aa7c21a049312d49bae997f39
Dev pathak <devpathak9685@gmail.com>
2026-08-05 23:24:12 +0530

    Binary Search(ShashCode)  - (QUESTIONS)-> (Leet-875 Koko Eating Bananas)

---
+++ src/binarySearch/ShashCode/Questions/KokoEatingBananas.java
@@ -0,0 +1,41 @@
+package binarySearch.ShashCode.Questions;
+
+import javax.swing.*;
+import java.util.Arrays;
+
+public class KokoEatingBananas {
+    public static void main(String[] args) {
+        int[] arr = {3,6,7,11};
+        System.out.println(minEatingSpeed(arr, 8));
+    }
+    public static  int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int start = 1;
+        int end = piles[piles.length-1];
+        int ans =-1;
+        while(start<=end){
+            int mid  = start+ (( end - start)/2);
+            if(ifEatingPossible(piles, mid , h)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return ans;
+
+    }
+    public static  boolean ifEatingPossible(int[] arr , int eatingSpeed, int totalHours){
+        int hourCount  =0;
+        for(int p : arr){
+            hourCount  += (p/eatingSpeed);
+            if(p%eatingSpeed!=0){
+                hourCount ++;
+            }
+            if(hourCount>totalHours){
+                return false;
+            }
+        }
+        return true;
+    }
+}
