df3157fb76b23b901d1e5b943868a66a9b73eb06
Dev pathak <devpathak9685@gmail.com>
2026-08-06 22:31:19 +0530

    Binary Search(ShashCode)  - (QUESTIONS)-> (Leet-69 Sqrt(x) (Square root using BS))

---
+++ src/binarySearch/ShashCode/Questions/SquareRootBS.java
@@ -0,0 +1,23 @@
+package binarySearch.ShashCode.Questions;
+
+public class SquareRootBS {
+    public static void main(String[] args) {
+
+    }
+    public int mySqrt(int x) {
+        int start = 1;
+        int end  = x;
+        while(start<=end){
+            int mid  = start + (end-start)/2;
+            if(mid > (x/mid)){
+                end = mid -1;
+            }else if(mid < (x/mid)){
+                start =mid +1;
+            }else{
+                return  mid;
+            }
+        }
+        return end;
+
+    }
+}
