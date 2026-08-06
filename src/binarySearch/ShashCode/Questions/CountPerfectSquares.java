dcc866ffe5c51bf7e5d5fe6d43bec4b48fcfcbe8
Dev pathak <devpathak9685@gmail.com>
2026-08-06 22:54:02 +0530

    Binary Search(ShashCode)  - (QUESTIONS)-> (GeeksForGeeks CountPerfectSquares)

---
+++ src/binarySearch/ShashCode/Questions/CountPerfectSquares.java
@@ -0,0 +1,29 @@
+package binarySearch.ShashCode.Questions;
+
+public class CountPerfectSquares {
+    public static void main(String[] args) {
+
+    }
+    static int countSquares(int n) {
+        // code here
+        int squareRoot = mySqrt(n);
+        return squareRoot-1;
+
+    }
+    public static int mySqrt(int n){
+        int start =1;
+        int end = n;
+        while(start<=end){
+            int mid  = start + (end-start)/2;
+            if(mid <= (n/mid)){
+                if(n%mid ==0 && mid == (n/mid)){
+                    return mid;
+                }
+                start = mid +1;
+            }else{
+                end = mid -1;
+            }
+        }
+        return start;
+    }
+}
