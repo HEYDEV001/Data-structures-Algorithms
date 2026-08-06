402a8aa985a892fb8b7eef92f576830647ba3b45
Dev pathak <devpathak9685@gmail.com>
2026-08-06 21:52:33 +0530

    Binary Search(ShashCode)  - (QUESTIONS)-> (Leet-540 Single Element in a Sorted Array)

---
+++ src/binarySearch/ShashCode/Questions/SingleNonDuplicate.java
@@ -0,0 +1,22 @@
+package binarySearch.ShashCode.Questions;
+
+public class SingleNonDuplicate {
+    public static void main(String[] args) {
+
+    }
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length-2;
+        while(start<=end){
+            int mid  = start + (end -start) / 2;
+            if(nums[mid] == nums[mid^1]){
+                start = mid +1;
+
+            }else{
+                end  = mid -1;
+            }
+        }
+        return nums[start];
+
+    }
+}
