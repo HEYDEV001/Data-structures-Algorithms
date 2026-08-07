0440366dcfcff58dfe6ed7c3333b1b5bc24a6561
Dev pathak <devpathak9685@gmail.com>
2026-08-08 00:05:32 +0530

    Interpolation Search(ShashCode)(Enhanced)

--- src/Searching/InterpolationSearch.java
+++ src/Searching/InterpolationSearch.java
@@ -2,13 +2,18 @@ package Searching;
 
 public class InterpolationSearch {
     public static void main(String[] args) {
-        int[] arr = {1,2 ,3 ,4 ,5 ,6, 7, 8,9,10, 11, 12, 13, 14, 15, 16};
-        System.out.println(interpolationSearch(arr, 10));
+        int[] arr = {1};
+        System.out.println(interpolationSearch(arr, 1));
     }
     public static int interpolationSearch(int[] arr, int target) {
         int start =0;
         int end = arr.length-1;
-        while(start <= end){
+        while(start <= end && target >= arr[start] && target <= arr[end]){
+            if(arr[start] == arr[end]){
+                if(target == arr[start]){
+                return start;
+                }
+            }
             int pos  = start + (target-arr[start])*(end -start)/(arr[end]-arr[start]);
               if(arr[pos] == target){
                   return pos;
