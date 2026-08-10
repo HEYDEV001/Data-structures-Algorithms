2636ce23375a824949d1641b3003be1dc15d9cab
Dev pathak <devpathak9685@gmail.com>
2026-08-10 23:58:27 +0530

    Sorting(ShashCode) pigeonHole  Sort

---
+++ src/SortingAlgos/PigeonHoleSort.java
@@ -0,0 +1,43 @@
+package SortingAlgos;
+
+import java.util.Arrays;
+
+public class PigeonHoleSort {
+    public static void main(String[] args) {
+        int [] arr  = {2, 34,5 ,3, 12,1 ,435 ,6 ,87};
+        System.out.println(Arrays.toString(pigeonholeSort(arr)));
+    }
+    public static int[] pigeonholeSort(int[] arr) {
+        int n = arr.length;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        if(max == Integer.MIN_VALUE || min == Integer.MAX_VALUE) {
+            return new int[0];
+        }
+        int size = max - min + 1;
+        int[] result = new int[size];
+
+        for (int i = 0; i < n; i++) {
+            int index = arr[i] - min;
+            result[index]++;
+        }
+        int insertionIndex = 0;
+        for (int j = 0; j < size; j++) {
+            while (result[j] > 0) {
+                arr[insertionIndex] = j + min;
+                insertionIndex++;
+                result[j]--;
+            }
+        }
+
+        return result;
+
+    }
+}
