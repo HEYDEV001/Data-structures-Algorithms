03616982e89fc1b028e036e23e825c6dd292e858
Dev pathak <devpathak9685@gmail.com>
2026-08-15 23:49:48 +0530

    Binary Search(ShashCode)  - (QUESTIONS)-> (GeeksForGeeks Anti clock matrix)

--- src/binarySearch/ShashCode/Questions/Matrix/AnticlockwiseMatrix.java
+++ src/binarySearch/ShashCode/Questions/Matrix/AnticlockwiseMatrix.java
@@ -2,10 +2,23 @@ package binarySearch.ShashCode.Questions.Matrix;
 
 import java.util.Arrays;
 
-public class TransposeMatrix {
+public class AnticlockwiseMatrix {
     public static void main(String[] args) {
         int[][] matrix = {{1,1,1},{2,2,2},{3,3,3}};
-        System.out.println(Arrays.deepToString(transpose(matrix)));
+        System.out.println(Arrays.deepToString(antiClockwise(matrix)));
+    }
+
+    public static int[][] antiClockwise(int[][] matrix) {
+        int m = matrix.length;
+        matrix = transpose(matrix);
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<m/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][m-1-j];
+                matrix[i][m-1-j ] = temp;
+            }
+        }
+        return matrix;
 
     }
     public static int[][] transpose(int[][] A) {
