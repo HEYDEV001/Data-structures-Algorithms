9501a53211d201d9d7c863015e72e54ff7e08ec1
Dev pathak <devpathak9685@gmail.com>
2026-08-22 23:39:26 +0530

    HashMap(ShashCode)  Question (GeeksForGeeks Array Subset of Another Array) updated

--- src/HashMap/Questions/ArraySubsetOfAnotherArray.java
+++ src/HashMap/Questions/ArraySubsetOfAnotherArray.java
@@ -5,22 +5,18 @@ import java.util.HashMap;
 public class ArraySubsetOfAnotherArray {
     public static void main(String[] args) {
 int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-int [] arr2 = {1, 2, 3, 4, 1};
+int [] arr2 = {1, 2, 3, 4,1};
         System.out.println(isSubset(arr, arr2));
     }
 
     public static boolean isSubset(int[] a, int[] b) {
-        // code here
-
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int j : a) {
             map.put(j, map.getOrDefault(j, 0) + 1);
         }
         for (int i : b) {
-            if (map.containsKey(i)) {
-                if (map.get(i) == 0) {
-                    return false;
-                }
+//            if (map.containsKey(i) && (map.get(i) > 0) ){
+                if ((map.getOrDefault(i, 0) > 0) ){
                 map.put(i, map.get(i) - 1);
             }else{
                 return false;
