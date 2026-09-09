e07f37a9a1d70d7d8988c7a87a8a3a9d3abc80ce
Dev pathak <devpathak9685@gmail.com>
2026-09-09 23:54:38 +0530

    Two pointers(ShashCode) Question (Leet-11 Trapped Rain water)

--- src/TwoPointers/ContainerWithMostWater.java
+++ src/TwoPointers/ContainerWithMostWater.java
@@ -1,2 +1,24 @@
-package TwoPointers;public class ContainerWithMostWater {
+package TwoPointers;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+
+    }
+    public static int maxWater(int [] height){
+        int i =0;
+        int j=height.length-1;
+        int ans = 0;
+        while(i<j){
+            int length = j - i;
+            int minHeight = Math.min(height[i],height[j]);
+            int volume = length * minHeight;
+            ans = Math.max(ans,volume);
+            if(height[i]<=height[j]){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return ans;
+    }
 }
