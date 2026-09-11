fb99948a2fe3f59b6531222349642acf0b78e595
Dev pathak <devpathak9685@gmail.com>
2026-09-11 23:08:09 +0530

    Two pointers(ShashCode) Question (Leet-509 Fibonacci Sequence)

---
+++ src/recursion/ShashCode/Questions/fibonacciSequence.java
@@ -0,0 +1,16 @@
+package recursion.ShashCode.Questions;
+
+public class fibonacciSequence {
+    public static void main(String[] args) {
+
+    }
+    public static  int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
