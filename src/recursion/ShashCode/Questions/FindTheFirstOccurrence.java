f16a88a2db77429ba5cf13dc725824549e949fcb
Dev pathak <devpathak9685@gmail.com>
2026-09-11 23:50:24 +0530

    Recursion(ShashCode) Question (Find First Occurrence)

---
+++ src/recursion/ShashCode/Questions/FindTheFirstOccurrence.java
@@ -0,0 +1,17 @@
+package recursion.ShashCode.Questions;
+
+public class FindTheFirstOccurrence {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(findTheFirstOccurrence(arr, 9, 0));
+    }
+    public static int findTheFirstOccurrence(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return -1;
+        }
+        if(arr[index] == target){
+            return index;
+        }
+        return findTheFirstOccurrence(arr, target, index+1);
+    }
+}
--- src/recursion/ShashCode/Questions/SearchInArray.java
+++ src/recursion/ShashCode/Questions/SearchInArray.java
@@ -3,14 +3,14 @@ package recursion.ShashCode.Questions;
 public class SearchInArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(searchInArray(arr, 90, 0));
+        System.out.println(searchInArray(arr, 10, 0));
 
     }
     public static boolean searchInArray(int[] arr, int target, int i) {
-        if(arr[i]==target){
-            return true;
-        }else if(i==arr.length-1){
+        if(i==arr.length){
             return false;
+        } else if(arr[i]==target){
+            return true;
         }
         return searchInArray(arr,target,i+1);
     }
