24eea73085e5515f9bc07dc8cdd15719824b30b5
Dev pathak <devpathak9685@gmail.com>
2026-09-12 00:26:02 +0530

    Recursion(ShashCode) Question (Print All the occurrences)

---
+++ src/recursion/ShashCode/Questions/PrintAllOccurrences.java
@@ -0,0 +1,19 @@
+package recursion.ShashCode.Questions;
+
+public class PrintAllOccurrences {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 4, 5, 6, 7, 2, 9, 10};
+        printAllOccurrences(arr, 0, 2);
+
+    }
+
+    public static void printAllOccurrences(int[] arr, int index, int target) {
+        if (index == arr.length) {
+            return;
+        }
+        if (arr[index] == target) {
+            System.out.print(index + " ");
+        }
+        printAllOccurrences(arr, index + 1, target);
+    }
+}
