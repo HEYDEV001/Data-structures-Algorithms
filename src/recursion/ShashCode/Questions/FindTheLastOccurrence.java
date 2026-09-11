776c6a0708c68b25882fd0896b224fdb5f2fdf35
Dev pathak <devpathak9685@gmail.com>
2026-09-11 23:59:18 +0530

    Recursion(ShashCode) Question (Find Last Occurrence 2)

--- src/recursion/ShashCode/Questions/FindTheLastOccurrence.java
+++ src/recursion/ShashCode/Questions/FindTheLastOccurrence.java
@@ -2,8 +2,9 @@ package recursion.ShashCode.Questions;
 
 public class FindTheLastOccurrence {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(findTheLastOccurrence(arr, 2, arr.length - 1));
+        int[] arr = {1, 2, 2, 4, 5, 6, 7, 2, 9, 10};
+        System.out.println(findTheLastOccurrence(arr, 20, arr.length - 1));
+        System.out.println(findTheLastOccurrence2(arr, 2, 0));
 
     }
     public static int findTheLastOccurrence(int[] arr, int target, int index) {
@@ -14,5 +15,18 @@ public class FindTheLastOccurrence {
             return index;
         }
         return findTheLastOccurrence(arr, target, index-1);
+    }
+
+    public static int findTheLastOccurrence2(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return -1;
+        }
+       int res  = findTheLastOccurrence2(arr, target, index+1);
+        if(res == -1){
+            if(arr[index] == target){
+                return index;
+            }
+        }
+        return res;
     }
 }
