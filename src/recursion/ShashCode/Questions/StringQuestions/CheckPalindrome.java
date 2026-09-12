4e6aed5d8fe207e0b94b28a57fc67c51b4900f52
Dev pathak <devpathak9685@gmail.com>
2026-09-12 22:59:58 +0530

    Recursion(ShashCode) Question (Check Palindrome & print reverse of a String)

---
+++ src/recursion/ShashCode/Questions/StringQuestions/CheckPalindrome.java
@@ -0,0 +1,19 @@
+package recursion.ShashCode.Questions.StringQuestions;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        String s = "asdfghjkllkjhgfds";
+        System.out.println(isPalindrome(s, 0, s.length() - 1));
+
+    }
+    public static boolean isPalindrome(String s, int start, int end ) {
+        if(start > end) {
+            return true;
+        }
+        if(s.charAt(start) != s.charAt(end)) {
+            return false;
+        }else{
+            return isPalindrome(s, start + 1, end - 1);
+        }
+    }
+}
--- src/recursion/ShashCode/Questions/StringQuestions/PrintString.java
+++ src/recursion/ShashCode/Questions/StringQuestions/PrintString.java
@@ -4,6 +4,7 @@ public class PrintString {
     public static void main(String[] args) {
         String string  = "STRING";
         printString(string, 0);
+        reverseString(string, 0);
 
     }
     public static void printString(String str, int index) {
@@ -12,5 +13,13 @@ public class PrintString {
         }
         System.out.print(str.charAt(index) + " ");
         printString(str, index + 1);
+    }
+
+    public static void reverseString(String  str, int index) {
+        if (index == str.length()) {
+            return;
+        }
+        reverseString(str, index + 1);
+        System.out.print(str.charAt(index) + " ");
     }
 }
