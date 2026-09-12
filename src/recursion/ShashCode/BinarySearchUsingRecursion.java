52a2b3e9d6c44fddaffdd4c547b407517b4a2a0b
Dev pathak <devpathak9685@gmail.com>
2026-09-12 20:36:38 +0530

    Recursion(ShashCode) Binary Search using Recursion

---
+++ src/recursion/ShashCode/BinarySearchUsingRecursion.java
@@ -0,0 +1,21 @@
+package recursion.ShashCode;
+
+public class BinarySearchUsingRecursion {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3 ,4 ,5 ,6, 7, 8, 9, 10};
+        System.out.println(binarySearch(arr,0,arr.length ,3));
+    }
+    public static int binarySearch(int[] arr,int start, int end, int target) {
+        if (start > end) {
+            return -1;
+        }
+        int mid = start + (end - start) / 2;
+        if(arr[mid] == target){
+            return mid;
+        }else if(arr[mid] < target){
+             return binarySearch(arr, mid+1, end, target);
+        }else {
+            return binarySearch(arr, start, mid-1, target);
+        }
+    }
+}
