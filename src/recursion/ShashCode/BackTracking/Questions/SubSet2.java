435476be3e70857b8bc3ffd4e5017b50a0b55cfa
Dev pathak <devpathak9685@gmail.com>
2026-09-14 23:52:35 +0530

    RecursionBackTracking(ShashCode) Question (Leet-78 Subsets II  (Backtracking) )

---
+++ src/recursion/ShashCode/BackTracking/Questions/SubSet2.java
@@ -0,0 +1,37 @@
+package recursion.ShashCode.BackTracking.Questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubSet2 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2};
+        System.out.println(subsetsWithDup(nums));
+    }
+    public static  List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        getSubSets(0,nums ,current,result);
+        return result;
+    }
+
+    public static void getSubSets(int index, int[] nums , List<Integer> current, List<List<Integer>> result){
+        if(index == nums.length){
+            result.add(new ArrayList(current));
+            return;
+        }
+        // Add
+        current.add(nums[index]);
+        //Pick
+        getSubSets(index +1,nums ,current,result);
+        // removing while when the recursive call is over
+        current.remove(current.size()-1);
+        // No pick
+        while((index+1) < nums.length && nums[index] == nums[index+1]){
+            index++;
+        }
+        getSubSets(index +1,nums ,current,result);
+    }
+}
