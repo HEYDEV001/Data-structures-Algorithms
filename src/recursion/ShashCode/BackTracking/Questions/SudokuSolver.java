1189ec5a25bd82987114de94ba7f2d9906469257
Dev pathak <devpathak9685@gmail.com>
2026-09-17 00:31:40 +0530

    RecursionBackTracking(ShashCode) Question (Leet-37. Sudoku Solver(Updated))

--- src/recursion/ShashCode/BackTracking/Questions/SudokuSolver.java
+++ src/recursion/ShashCode/BackTracking/Questions/SudokuSolver.java
@@ -5,47 +5,44 @@ public class SudokuSolver {
 
     }
     public void solveSudoku(char[][] board) {
-        backtrack(board,0,0);
+        solve( board,0,0);
     }
-    private boolean backtrack(char board[][], int row, int col){
-        //base case
-        if(col == 9){
-            col =0;
+    private boolean solve(char[][] board, int row, int col){
+        if(col==9){
+            col=0;
             row = row+1;
         }
         if(row == 9){
             return true;
         }
-        //if already filled
-        if(board[row][col]!='.') {
-            return backtrack(board, row, col+1);
+        // If the position is already filled
+        if(board[row][col] != '.'){
+            return solve(board, row, col+1);
         }
-
-        for(int d=1;d<=9;d++){
+        for(int d = 1 ; d<=9; d++){
             char digit = (char)(d+'0');
-            if(isPlacementPossible(digit, board, row, col)){
-                board[row][col] = digit;
-                if(backtrack(board,row,col+1)){
+            if(isPlacementPossible(board, digit, row, col)){
+                board[row][col]=digit;
+                if(solve(board, row, col+1)){
                     return true;
                 }
-                board[row][col] = '.';
+                board[row][col]='.';
             }
         }
         return false;
     }
-    private boolean isPlacementPossible(char digit, char board[][], int row, int col){
-        // current row
-        //current col
-        // grid
-        for(int j=0;j<9;j++){
-            //current row
-            if(board[row][j] == digit) return false;
-            //current col
-            if(board[j][col] == digit) return false;
-            //grid
+
+    private boolean isPlacementPossible(char[][] board, char digit, int row, int col){
+        for(int j =0 ; j<9;j++){
+            // current col
+            if(board[j][col]==digit) return false;
+            // current row
+            if(board[row][j]== digit) return false;
+
             int r = 3*(row/3) + (j/3);
-            int c = 3*(col/3) + (j%3);
-            if(board[r][c] == digit) return false;
+            int c = 3*(col/3)+(j%3);
+            if(board[r][c]== digit) return false;
+
         }
         return true;
     }
