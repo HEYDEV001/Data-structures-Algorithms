a92c39a1fdb42c8680cc9ee72ac07e9259267b3f
Dev pathak <devpathak9685@gmail.com>
2026-09-21 17:10:55 +0530

    Dynamic Programming(ShashCode) Question (Leet-746 Min Cost Climbing Stairs(using Tabulation & Space Optimization))

--- src/DynamicProgramming/Questions/MinCostClimbingStairs.java
+++ src/DynamicProgramming/Questions/MinCostClimbingStairs.java
@@ -43,4 +43,36 @@ public class MinCostClimbingStairs {
         return dp[index];
     }
 
+    // // bottom-up
+     public int minCostClimbingStairs3(int[] cost) {
+         int n  =  cost.length;
+         int[] dp = new int[n+1];
+         if(n == 0 || n == 1){
+             return 0;
+         }
+         dp[0] = 0;
+         dp[1] = 0;
+         for( int state = 2; state <=n; state++){
+             dp[state] = Math.min((cost[state-1]  +  dp[state-1]), (cost[state-2]  +  dp[state-2]));
+         }
+         return dp[n];
+     }
+
+    // bottom-up (Space Optimised)
+    public int minCostClimbingStairs4(int[] cost) {
+        int n  =  cost.length;
+        if(n == 0 || n == 1){
+            return 0;
+        }
+        int prev0 = 0;
+        int prev1 = 0;
+        int ans = 0;
+        for( int state = 2; state <=n; state++){
+            ans= Math.min((cost[state-1]  +  prev1), (cost[state-2]  +  prev0));
+            prev0 = prev1;
+            prev1 = ans;
+        }
+        return ans;
+    }
+
 }
