550f32a5d4329b37f7a20cb01b2cd560256e9a9a
Dev pathak <devpathak9685@gmail.com>
2026-09-21 18:51:52 +0530

    Dynamic Programming(ShashCode) Question (Leet-198. House Robber(using Tabulation & Space Optimization))

--- src/DynamicProgramming/Questions/Medium/HouseRobber.java
+++ src/DynamicProgramming/Questions/Medium/HouseRobber.java
@@ -46,4 +46,50 @@ public class HouseRobber {
         dp[index] = Math.max(pick , noPick);
         return dp[index];
     }
+     // Bottom-up (Tabulation)
+     // Used Shifting of dp array
+     // dp[1] = nums[0] -> dp[index] = nums[index-1] or dp[index+1] = nums[index]
+     public int rob3(int[] nums) {
+         int n = nums.length;
+         int[] dp  = new int[n+1];
+         if(n == 0){
+             return nums[0];
+         }
+         if(n == -1){
+             return 0;
+         }
+         dp[1] = nums[0];
+         dp[0]  = 0;
+         for(int state =2; state<=n ; state++){
+             int pick = nums[state-1] + dp[state-2];
+             int noPick = dp[state-1];
+             dp[state] = Math.max(pick , noPick);
+         }
+         return dp[n];
+     }
+
+    // Bottom-up (Tabulation)(Space Optimization)
+    // Used Shifting of dp array
+    // dp[1] = nums[0] -> dp[index] = nums[index-1] or dp[index+1] = nums[index]
+    public int rob4(int[] nums) {
+        int n = nums.length-1;
+        if(n == 0){
+            return nums[0];
+        }
+        if(n == -1){
+            return 0;
+        }
+        int previous = nums[0];
+        int beforePrevious  = 0;
+        int ans = 0;
+        for(int state =2; state<=n+1 ; state++){
+            int pick = nums[state-1] + beforePrevious;
+            int noPick = previous;
+            ans = Math.max(pick , noPick);
+            beforePrevious = previous;
+            previous  = ans ;
+
+        }
+        return ans;
+    }
 }
