e6ac3abc7b504ea899b949076a22eb7a60af1a98
Dev pathak <devpathak9685@gmail.com>
2026-09-24 23:21:59 +0530

    Dynamic Programming(ShashCode) Question (GeeksForGeeks 0-1 Knapsack Problem(using Tabulation))

--- src/DynamicProgramming/Questions/Medium/ZeroOneKnapSack.java
+++ src/DynamicProgramming/Questions/Medium/ZeroOneKnapSack.java
@@ -68,4 +68,33 @@ public class ZeroOneKnapSack {
         dp[index][cap] = Math.max(pick, noPick);
         return dp[index][cap];
     }
+
+    // DP(Memoization)
+    public int knapsack3(int cap, int val[], int wt[]) {
+        // code here
+        int n  = val.length-1;
+        int[][] dp = new int[n+1][cap+1];
+        for (int c = 0; c <= cap; c++) {
+            if(wt[0] <= c){
+                dp[0][c] = val[0];
+            }else{
+                dp[0][c] = 0;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int c = 1; c <= cap; c++) {
+                int pick = 0;
+                if(wt[i] <= c){
+                    pick = val[i] +  dp[i-1][c-wt[i]];
+                }
+                int noPick =  dp[i-1][c];
+                dp[i][c] = Math.max(pick, noPick);
+            }
+        }
+        return dp[n][cap];
+
+    }
 }
