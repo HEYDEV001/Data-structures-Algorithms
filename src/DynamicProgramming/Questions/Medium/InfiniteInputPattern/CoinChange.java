a4aa25566c8075f090a17d7fce0b0785c8a68fbc
Dev pathak <devpathak9685@gmail.com>
2026-09-25 00:19:51 +0530

    Dynamic Programming(ShashCode) Question (Leet-322 Coin Change(using Memoization))

--- src/DynamicProgramming/Questions/Medium/InfiniteInputPattern/CoinChange.java
+++ src/DynamicProgramming/Questions/Medium/InfiniteInputPattern/CoinChange.java
@@ -1,5 +1,7 @@
 package DynamicProgramming.Questions.Medium.InfiniteInputPattern;
 
+import java.util.Arrays;
+
 public class CoinChange {
     public static void main(String[] args) {
 
@@ -30,5 +32,40 @@ public class CoinChange {
         }
         int noPick = result(coins, amount, index-1);
         return Math.min(pick, noPick);
+    }
+
+    // Using DP(Memoization)
+    public static int coinChange2(int[] coins, int amount) {
+        int n = coins.length -1;
+        int[][] dp = new int[n+1][amount + 1];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        int res = result(coins, amount, n, dp);
+        if(res == (int)(1e9)){
+            return -1;
+        }else{
+            return res;
+        }
+    }
+    private static int result(int[] coins, int amount, int index, int[][] dp){
+        if(index == 0){
+            if(amount % coins[index] == 0){
+                dp[index][amount] = amount/coins[0];
+                return dp[index][amount];
+            }
+            dp[index][amount] = (int)(1e9);
+            return dp[index][amount];
+        }
+        if(dp[index][amount] != -1){
+            return dp[index][amount];
+        }
+        int pick = (int)(1e9);
+        if(coins[index] <= amount){
+            pick = 1 + result(coins, amount - coins[index], index, dp);
+        }
+        int noPick = result(coins, amount, index-1, dp);
+        dp[index][amount] = Math.min(pick, noPick);
+        return dp[index][amount];
     }
 }
