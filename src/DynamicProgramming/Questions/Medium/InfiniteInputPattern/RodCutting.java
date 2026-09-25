9dbb8e3c0ff54d626e851554a99fc75ac53d7a06
Dev pathak <devpathak9685@gmail.com>
2026-09-25 22:00:42 +0530

    Dynamic Programming(ShashCode) Question (GFG Rod Cutting(using Tabulation  & T + space Optimisation))

--- src/DynamicProgramming/Questions/Medium/InfiniteInputPattern/RodCutting.java
+++ src/DynamicProgramming/Questions/Medium/InfiniteInputPattern/RodCutting.java
@@ -57,4 +57,51 @@ public class RodCutting {
         dp[index][n] = Math.max(pick , noPick);
         return dp[index][n];
     }
+
+    // Tabulation
+    public int cutRod3(int[] price) {
+        // code here
+        int n = price.length;
+        int [][]dp = new int[n][n+1];
+        for(int l = 0;  l<n+1; l++){
+            dp[0][l] = l * price[0];
+        }
+        for(int i = 1; i < n; i++){
+            for(int l = 0; l <= n; l++){
+                int pick  = 0;
+                int currentLength = i + 1;
+                if(l >= currentLength){
+                    pick = price[i] + dp[i][l-currentLength];
+                }
+                int noPick = dp[i-1][l];
+                dp[i][l] = Math.max(pick , noPick);
+            }
+        }
+        return dp[n-1][n];
+    }
+
+    // Tabulation + space Optimisation
+    public int cutRod4(int[] price) {
+        // code here
+        int n = price.length;
+        int []prev = new int[n+1];
+        for(int l = 0;  l<n+1; l++){
+            prev[l] = l * price[0];
+        }
+        for(int i = 1; i < n; i++){
+            int[] current = new int[n+1];
+            for(int l = 0; l <= n; l++){
+                int pick  = 0;
+                int currentLength = i + 1;
+                if(l >= currentLength){
+                    pick = price[i] + current[l-currentLength];
+                }
+                int noPick = prev[l];
+                current[l] = Math.max(pick , noPick);
+            }
+            prev = current;
+        }
+        return prev[n];
+    }
+
 }
